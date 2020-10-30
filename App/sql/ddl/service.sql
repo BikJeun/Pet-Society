11504ac1922b4d89852e611170134a100a3b12cd
Ong Bik Jeun <e0426272@u.nus.edu>
2020-10-30 13:39:15 +0800

    useless

--- App/sql/ddl/service.sql
+++ App/sql/ddl/service.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS service cascade;
 
 CREATE TABLE service (
-    pickup DATETIME NOT NULL PRIMARY KEY,
-    dropoff DATETIME NOT NULL
+    pickup DATETIME NOT NULL ,
+    duration INT NOT NULL,
+    price DECIMAL NOT NULL,
     staff VARCHAR(100) NOT NULL REFERENCES caretaker,
+    PRIMARY KEY(pickup, duration, price)
 );
