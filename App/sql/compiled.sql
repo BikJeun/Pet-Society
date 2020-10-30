8cba4fcd5f1e27629a9de54311ae2de4796c2b0b
Ong Bik Jeun <e0426272@u.nus.edu>
2020-10-30 20:34:41 +0800

    Added more dml statements and triggers

--- App/sql/compiled.sql
+++ App/sql/compiled.sql
@@ -139,3 +139,127 @@ CREATE TRIGGER check_fulltimer
 BEFORE INSERT ON fulltimer
 FOR EACH ROW EXECUTE PROCEDURE not_parttimer();
 -----------------------------------------------------------------------------------
+
+-- Trigger to make sure ratings are made properly
+CREATE OR REPLACE FUNCTION check_ratings()
+RETURNS TRIGGER AS
+$$ DECLARE ctx NUMERIC;
+    BEGIN
+        -- Ensure that ratings are done after service completes
+        IF NOT EXISTS (SELECT 1 FROM bid_service b WHERE b.accepted = true AND DATEADD(day, b.duration, b.pickup_date) < CURRENT_TIMESTAMP) 
+        THEN RAISE EXCEPTION 'Ratings are not allowed to be made for an ongoing service';
+        RETURN NULL;
+        -- Ensure ratings are between 0-5
+        ELSE IF NEW.rating > 5 OR NEW.rating < 0 THEN
+            RAISE EXCEPTION 'Ratings must be within 0-5.';
+            RETURN NULL;
+        ELSE
+            RETURN NEW;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER rating_trigger 
+BEFORE INSERT ON ratings
+FOR EACH ROW EXECUTE PROCEDURE check_ratings();
+
+-------------------------------------------------------------------------------------
+
+--Trigger to make sure caretakers take up services they can take
+CREATE OR REPLACE FUNCTION check_service()
+RETURNS TRIGGER AS
+$$ DECLARE ctx NUMERIC;
+    BEGIN
+        IF(TG_OP = 'INSERT') OR (TG_OP = 'UPDATE') THEN
+            --CHECK NUMBER OF SERVICES BY CARETAKER OVERLAP
+            SELECT COUNT(*) AS count
+            FROM bid_service b
+            WHERE b.care_taker_email <> NEW.care_taker_email
+            AND b.pet_owner_email =  NEW.pet_owner_email
+            AND b.pickup_date <= DATEADD(day, NEW.duration, NEW.pickup_date)
+            AND NEW.pickup_time <= DATEADD(day, b.duration, b.pickup_date);
+            -- CHECK IF SERVICE EXCEED (FULLTIMER)
+            IF EXIST(SELECT 1 FROM fulltimer) AND count > 5 THEN
+                RAISE EXCEPTION 'There is a limit of up to 5 pets at any one time';
+                RETURN NULL;
+            -- CHECK IF SERVICE EXCEED (PARTTIMER)
+            ELSE IF EXIST(SELECT 1 FROM parttimer) AND RATINGS < 4 AND COUNT > 2 THEN
+                RAISE EXCEPTION 'You cannot have moret than 2 pet at your care at any one time';
+                RETURN NULL;
+            ELSE IF NEW.price < 0 OR NEW.price IS NULL THEN
+                RAISE EXCEPTION 'Price cannot be null or negative';
+                RETRUN NULL;
+            ELSE 
+                RETURN NEW;
+            END IF;
+        END IF;
+        ELSE IF (TG_OP = 'DELETE') THEN
+            -- CHECK IF CARETAKER CANCELS SERVICES 1 DAY BEFORE
+            IF EXISTS(SELECT 1 FROM bid_service b
+                        WHERE b.care_taker_email = OLD.care_taker_email
+                        AND b.accepted = true;)
+                        AND OLD.pickup_time - CURRENT_TIMESTAMP < INTERVAL '1 DAY' THEN
+                RAISE EXCEPTION 'You cannot cancel one day before your service time!';
+                RETURN NULL;
+            ELSE
+                RETURN NEW;
+            END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER bid_service_trigger
+BEFORE INSERT OR UPDATE OR DELETE ON bid_service
+FOR EACH ROW EXECUTE PROCEDURE check_service();
+---------------------------------------------------------------
+--Trigger to allow for acceptance of job immediately
+
+CREATE OR REPLACE FUNCTION accept_bid()
+RETURNS TRIGGER AS 
+$$ DECLARE ctx NUMERIC;
+    BEGIN 
+        --FULLTIMERS WILL AUTO ACCEPT BID IF POSSIBLE
+        IF EXIST(SELECT 1 FROM fulltimer f WHERE f.email = NEW.care_taker_email)
+            AND SELECT COUNT(*) AS count
+            FROM bid_service b
+            WHERE b.care_taker_email <> NEW.care_taker_email
+            AND b.pet_owner_email =  NEW.pet_owner_email
+            AND b.pickup_date <= DATEADD(day, NEW.duration, NEW.pickup_date)
+            AND NEW.pickup_time <= DATEADD(day, b.duration, b.pickup_date) < 5 THEN
+            RAISE EXCEPTION 'Bid service accepted automatically'
+            NEW.accepted = true;
+            RETURN NEW;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER auto_accept
+BEFORE INSERT OR UPDATE ON bid_service
+FOR EACH ROW EXECUTE PROCEDURE accept_bid();
+
+
+
+---------------------------------------------------------------
+--Trigger to check if leave is possible
+CREATE OR REPLACE FUNCTION check_leave()
+RETURNS TRIGGER AS
+$$ DECLARE ctx NUMERIC;
+    BEGIN
+        --applying for leave during service period
+        IF EXIST(SELECT 1 FROM bid_service B
+                    WHERE b.care_giver_email = NEW.care_giver_email
+                    AND (NEW.start_date <= b.pickup_date)
+                        OR (NEW.start_date <= DATEADD(day, b.duration, b.pickup_date))
+                        OR NEW.end_date >= b.pickup_date)) THEN
+            RAISE EXCEPTION 'You have a job offer!';
+            RETURN NULL;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER leave_trigger
+BEFORE INSERT ON leave
+FOR EACH ROW EXECUTE PROCEDURE check_leave();
+
+
+            
+            
--- App/sql/index.js
+++ App/sql/index.js
@@ -9,6 +9,9 @@ sql.query = {
     
     //PCSAdmin
     create_pcs_admin: 'INSERT INTO PCSAdmin (email, password, name, address, zone) VALUES ($1,$2,$3,$4,$5) RETURNING *',
+    update_pcs_admin_email: 'UPDATE PCSAdmin a SET a.email = $2 WHERE a.email = $1',
+    update_pcs_admin_password: 'UPDATE PCSAdmin a SET a.password = $2 WHERE a.email = $1',
+    update_pcs_admin_address: 'UPDATE PCSAdmin a SET a.address = $2, a.zone = $3 WHERE a.email = $1',
 
     //Caretakers
     create_care_taker: 'INSERT INTO caretaker (email, password, name, address, zone) VALUES ($1,$2,$3,$4,$5) RETURNING *',  
@@ -17,10 +20,17 @@ sql.query = {
     retrieve_all_care_taker_by_zone: 'SELECT c.email, c.name, c.address, c.zone, c.password FROM caretaker c WHERE c.zone = $1 LIMIT 25',
     retrieve_all_full_time_care_taker_by_zone: 'SELECT c.email, c.name, c.address, c.zone FROM caretaker c INNER JOIN fulltimer f ON c.email = f.email WHERE c.zone = $1 LIMIT 25',
     retrieve_all_part_time_care_taker_by_zone: 'SELECT c.email, c.name, c.address, c.zone FROM caretaker c INNER JOIN parttimer p ON c.email = p.email WHERE c.zone = $1 LIMIT 25',
+    update_care_taker_email: 'UPDATE caretaker c SET c.email = $2 WHERE c.email = $1',
+    update_care_taker_password: 'UPDATE caretaker c SET c.password = $2 WHERE c.email = $1',
+    update_care_taker_address: 'UPDATE caretaker c SET c.address = $2, c.zone = $3 WHERE c.email = $1',
+
 
     //PetOwners
     create_pet_owner: 'INSERT INTO petowner (email, password, name, address, zone) VALUES ($1,$2,$3,$4,$5) RETURNING *',
     retrieve_all_pet_owner_by_zone: 'SELECT email, name, address, zone FROM petowner WHERE zone = $1 LIMIT 25',
+    update_pet_owner_email: 'UPDATE petowner p SET p.email = $2 WHERE p.email = $1',
+    update_pet_owner_password: 'UPDATE petowner p SET p.password = $2 WHERE p.email = $1',
+    update_pet_owner_address: 'UPDATE petowner p SET p.address = $2, pc.zone = $3 WHERE p.email = $1',
 
     //Pet
     create_pet: 'INSERT INTO pet (pet_owner_email, pet_name, pet_type, special_requirements) VALUES ($1, $2, $3, $4) RETURNING *',
@@ -31,24 +41,53 @@ sql.query = {
     retrieve_pet_filter_by_type_and_owner: 'SELECT * FROM pet p WHERE p.pet_owner_email = $1 AND p.pet_type = $2',
     // delete_one: 'DELETE FROM pet p WHERE p.petName = $1 AND p.petOwner = $2', // hold this for later on because cannot anyhow delete
     update_requirements: 'UPDATE pet p SET special_requirements = $4 WHERE p.pet_name = $1 AND p.pet_owner_email = $2 AND p.pet_type = $3',
+    retrieve_pet_filter_by_base_daily_price:'SELECT * FROM pet p WHERE base_daily_price <=$1',
 
     //Bids
     create_bid: 'INSERT INTO bid_service (pet_owner_email, pet_name, pet_type, pickup_date, duration, price) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
     accept_bid: 'UPDATE bid_service SET accepted = true WHERE pet_owner_email = $1 AND pet_name = $2 AND pet_type = $3 AND pickup_date = $4 AND duration = $5 AND price = $6',
-    retrieve_bid_filter_by_date_and_price: 'SELECT * FROM bid_service WHERE ', //STOPPED HERE
-
-    filter_by_service: 'SELECT p.email, p.firstName, p.lastName, b.pet, b.price FROM bidsview b INNER JOIN petOwner p ON b.petOwner = p.email WHERE b.petPickUp = $1',
-    filter_by_service_arranged_by_top_bids: 'SELECT p.email, p.firstName, p.lastName, b.pet, b.price FROM bidsview b INNER JOIN petOwner p ON b.petOwner = p.email WHERE b.petPickUp = $1 ORDER BY b.price DESC LIMIT $2',
-    retrieve_all_bids: 'SELECT s.pickup, s.dropoff, b.pet, b.price, b.status FROM bidsview b INNER JOIN service s ON b.petPickUp = s.pickup ORDER BY s.caretaker ',
-    filter_by_owner: 'SELECT s.pickup, s.dropoff, b.pet, b.price, b.status FROM bidsview b INNER JOIN service s ON b.petPickUp = s.pickup WHERE b.petOwner = $1 ORDER BY s.caretaker ',
-    filter_by_caretaker_bids: 'SELECT s.pickup, s.dropoff, b.pet, b.price, b.petOwner, c.firstName, c.lastName, b.status FROM (bidsview b INNER JOIN service s ON b.petPickUp = s.pickup) INNER JOIN caretaker c ON c.email = b.petOwner WHERE S.caretaker = $1 ORDER BY s.pickup',
-    filter_by_accepted_service: 'SELECT s.pickup, s.dropoff, b.pet, b.price FROM bidsview B INNER JOIN service s ON b.petPickUp = s.pickup WHERE s.caretaker = $1 AND b.status = "accepted" ',
+    retrieve_bid_filter_by_date_and_price: 'SELECT * FROM bid_service WHERE pickup_date = $1 ORDER BY price ASC ', //SHOULD THIS BE JUST RETREIVE BY DATE?
+    retrieve_bid_filter_by_accepted_status: 'SELECT * FROM bid_service WHERE accepted = "true"',
+    retrieve_bid_filter_by_pending_status: 'SELECT * FROM bid_service WHERE accepted = "false"',
+    retrieve_bid_filter_by_owner: 'SELECT * FROM bid_service WHERE pet_owner_email = $1 ORDER BY care_taker_email',
+    retrieve_bid_filter_by_caretaker_bids: 'SELECT * FROM bid_service WHERE care_taker_email = $1',
+    retrieve_all_bids: 'SELECT * FROM bid_service ORDER BY care_taker_email',
+    retrieve_bid_filter_by_service_arranged_by_top_bids: 'SELECT * FROM bid_service WHERE pickup_date = $1 ORDER BY price DESC LIMIT $2',
+    retrieve_bid_filter_by_transport_agreement: 'SELECT * FROM bid_service WHERE transport_agreement = $1',
+    retrieve_bid_grouped_by_transport_agreement: 'SELECT * FROM bid_service GROUP BY transport_agreement',
+    retrieve_care_taker_filter_by_ratings: 'SELECT b.care_taker_email FROM bid_service b WHERE ratings >= $1 ',
+    retreive_review_filter_by_care_taker: 'SELECT b.review FROM bid_service b WHERE b.care_taker_email = $1',
+    retreive_bid_filter_by_month: 'SELECT * FROM bid_service b WHERE accepted = "true" AND EXTRACT(MONTH FROM pickup_date) = $1',
+    //cancel bid?
 
     //Rating
-    retrieve_avg_rating: 'SELECT ROUND(AVG(s.rating),2) AS average, COUNT(*) AS NUMBER FROM serviceProvider s GROUP BY staff HAVING staff = $1'
+    retrieve_avg_rating: 'SELECT ROUND(AVG(s.rating),2) AS average, COUNT(*) AS NUMBER FROM bid_service s GROUP BY s.care_taker_email HAVING s.care_taker_email = $1'
 
+    //FROM THIS POINT ON THE QUERIES ARE NOT TESTED (i'll leave it not as one line so u can check easily)
 
+    //Price
+     retrive_base_price_for_fulttimer: 'SELECT f.care_taker_email, b.pet_type, CASE WHEN (SELECT ROUND(AVG(s.rating),2) FROM bid_service s WHERE s.care_taker_email $1) >= 4 THEN (p.base_daily_price * 1.5) AS price WHEN (SELECT ROUND(AVG(s.rating),2) FROM bid_service s WHERE s.care_taker_email $1) = 3 THEN (p.base_daily_price * 1.25) AS price WHEN(SELECT ROUND(AVG(s.rating),2) FROM bid_service s WHERE s.care_taker_email $1)  < 3  THEN p.base_daily_price AS price FROM (bid_service b JOIN caretaker c ON b.care_taker_email = c.email) JOIN pet p ON b.pet_type = p.pet_type WHERE c.email NOT EXIST (SELECT 1 FROM parttimer pt WHERE pt.email = c.email) UNION SELECT f.email, p.pet_type, p.base_daily_price AS price FROM fulltimer f, pet p WHERE f.email NOT EXIST(SELECT 1 FROM bid_service WHERE f.email = care_taker_email)',
 
+    //Salary --->for total salary probably can use CTE?
+    //(CARETAKER SIDE)
+    retrieve_parttimer_salary: 'SELECT b.care_taker_email, SUM(0.75 * price) AS salary FROM bid_service b GROUP BY b.care_taker_email HAVING b.care_taker_email EXIST(SELECT 1 FROM parttimer p WHERE b.care_taker_email = p.email) WHERE b.accepted = "true" AND EXTRACT(MONTH FROM b.pickup_date) = $1' ,
+    retrieve_number_of_pet_days_for_fulltimer_monthly: 'SELECT COUNT(*) AS number FROM bid_service GROUP BY b.care_taker_email HAVING b.care_taker_email EXIST(SELECT 1 FROM fulltimer f WHERE b.care_taker_email = f.email) WHERE b.accepted = "true" AND EXTRACT(MONTH FROM b.pickup_date) = $1',
+    //INCOMPLETE --> mindblock on calculating it
+    retrieve_fulltimer_salary: 'SELECT b.care_taker_email, 
+    CASE
+    WHEN (SELECT COUNT(*) AS number FROM bid_service GROUP BY b.care_taker_email HAVING b.care_taker_email EXIST(SELECT 1 FROM fulltimer f WHERE b.care_taker_email = f.email) WHERE b.accepted = "true" AND EXTRACT(MONTH FROM b.pickup_date) = $1 ) <= 60 THEN 3000 AS salary
+    WHEN (SELECT COUNT(*) AS number FROM bid_service GROUP BY b.care_taker_email HAVING b.care_taker_email EXIST(SELECT 1 FROM fulltimer f WHERE b.care_taker_email = f.email) WHERE b.accepted = "true" AND EXTRACT(MONTH FROM b.pickup_date) = $1)  
+
+    //tOTAL NUMBER OF PET (PCS ADMIN SIDE)
+    retrive_total_number_of_pet_taken_care_in_a_month: 'SELECT COUNT(*) AS number, b.pet_type FROM bid_service b GROUP BY b.pet_type',
+    
+    //highest number of jobs
+    retrieve_month_with_highest_number_of_jobs: 'SELECT month FROM (SELECT EXTRACT(MONTH FROM pickup_date) AS month, COUNT(*) AS number FROM bid_service GROUP BY EXTRACT(MONTH FROM pickup_date)) WHERE number = MAX(number)',
+
+    //(PET OWNER SIDE)
+    retrieve_all_care_taker_in_their_area: 'SELECT c.name, c.email FROM caretaker c WHERE c.zone = $1',
+    retreive_other_pet_owner_nearby: 'SELECT p.name, p.email, p.address FROM petowner p WHERE p.zone = $1',
+    
 
 
 
